9ce0e025c9c92d4423de490069351ee3345402c9
Anil Madhavapeddy <anil@recoil.org>
2018-07-04 00:10:28 +0100

    reset vendor

--- vendor/csv/CHANGES.md
+++
@@ -1,5 +0,0 @@
-2.0 2017-09-02
---------------
-
-- Split the package into `csv` and `csv-lwt`.
-- Port to `jbuilder` and `topkg`.
--- vendor/extlib/test/runner.ml
+++
@@ -1,9 +0,0 @@
-(** test runner *)
-
-let () =
-  let filter =
-    match Array.to_list Sys.argv with
-    | [] | [_] -> None
-    | _::l -> Some (List.map String.lowercase l)
-  in
-  exit (if Util.run_all filter then 0 else 1)
