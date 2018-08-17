280f125c017dac3576729ceb29b6ebd48ebd68ee
Anil Madhavapeddy <anil@recoil.org>
2018-08-17 19:06:29 +0100

    Squashed 'vendor/odoc/' changes from ee6b0f2c0..60ee4fbbc
    60ee4fbbc Build: convert from Jbuilder to Dune
    5934f0fa3 Development build: compatibility with Dune 1.1.0
    3beb81352 Tests: move dummy static assets
    9416af5a0 Travis: test on 4.07.0 instead of 4.07.0+beta2
    88cec7eef Rename --lang to --syntax parameter (#163)
    7f27e13eb Add Reason syntax support (#156)
    dd47a98e7 Merge pull request #154 from rizo/add_theme_uri_option
    991d61fe5 Fix markup.ml dev dependency (#157)
    69460a645 Add more theme tests. Rename all occurences of assets.
    27d15268a Add support-files command. Rename 'assets' to 'support files'.
    e226b964f Promote custom theme test to use output-dir for the JS file.
    9fc47ec33 Don't use `theme_uri` for `highlight.pack.js`.
    6686a173e Add support for relative theme URIs.
    597953074 Install the default theme assets for odig.
    8b219df66 Add a test for custom theme URI.
    aba92bcd8 Add --theme-uri option to support custom HTML assets.
    
    git-subtree-dir: vendor/odoc
    git-subtree-split: 60ee4fbbc9e9311f9ccd3a3436489f7d14ee57eb

--- src/odoc/html_page.mli
+++ src/odoc/html_page.mli
@@ -14,10 +14,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
+
 (** Produces .html files from a .odoc file. *)
 
 val from_odoc :
-  env:Env.builder -> output:Fs.Directory.t -> Fs.File.t -> unit
+  env:Env.builder -> ?syntax:Html.Html_tree.syntax -> ?theme_uri:Html.Html_tree.uri -> output:Fs.Directory.t ->
+  Fs.File.t -> unit
 
-val from_mld : env:Env.builder -> package:Model.Root.Package.t ->
+val from_mld : env:Env.builder -> ?syntax:Html.Html_tree.syntax -> package:Model.Root.Package.t ->
   output:Fs.Directory.t -> Fs.File.t -> unit
