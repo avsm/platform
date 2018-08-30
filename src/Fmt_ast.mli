de9ef67f833a76f4d8264f2dc17b6a77d98fb8f3
Anil Madhavapeddy <anil@recoil.org>
2018-08-30 12:32:43 +0100

    Squashed 'vendor/ocamlformat/' content from commit a1bbe9d62
    git-subtree-dir: vendor/ocamlformat
    git-subtree-split: a1bbe9d62be79cb4a4aad3ea1d1940330f34fa44

---
+++ src/Fmt_ast.mli
@@ -0,0 +1,26 @@
+(**********************************************************************
+ *                                                                    *
+ *                            OCamlFormat                             *
+ *                                                                    *
+ *  Copyright (c) 2017-present, Facebook, Inc.  All rights reserved.  *
+ *                                                                    *
+ *  This source code is licensed under the MIT license found in the   *
+ *  LICENSE file in the root directory of this source tree.           *
+ *                                                                    *
+ **********************************************************************)
+module Format = Format_
+
+(** Format OCaml Ast *)
+
+open Migrate_ast
+open Parsetree
+
+val fmt_signature : Source.t -> Cmts.t -> Conf.t -> signature -> Fmt.t
+(** Format a signature. *)
+
+val fmt_structure : Source.t -> Cmts.t -> Conf.t -> structure -> Fmt.t
+(** Format a structure. *)
+
+val fmt_use_file :
+  Source.t -> Cmts.t -> Conf.t -> toplevel_phrase list -> Fmt.t
+(** Format a use_file. *)
