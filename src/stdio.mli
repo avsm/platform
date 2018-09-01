d9772d7316ca989016edb64a165da5affb8f6ab5
Anil Madhavapeddy <anil@recoil.org>
2018-09-01 01:01:57 +0100

    Squashed 'vendor/stdio/' changes from e8c1cb8e6..6513e42ca
    6513e42ca v0.11.119.01+59
    2529bf3f2 v0.11.117.19+55
    218ca1f03 v0.11.117.17+10
    aa18eb56e v0.11.117.09+15
    c6098aa4a v0.11.117.08+54
    d2a49f14d v0.11.117.03+59
    42abea9b2 v0.11.117.00+101
    REVERT: e8c1cb8e6 v0.11.0
    
    git-subtree-dir: vendor/stdio
    git-subtree-split: 6513e42cab33b9fde2f389e0a933197cf2b8cf82

--- src/stdio.mli
+++ src/stdio.mli
@@ -16,8 +16,14 @@ val stderr : Out_channel.t
 (** Same as {!Out_channel.printf} *)
 val printf : ('a, Out_channel.t, unit) format -> 'a
 
+(** Same as {!Out_channel.print_s} *)
+val print_s : ?mach : unit -> Sexp.t -> unit
+
 (** Same as {!Out_channel.eprintf} *)
 val eprintf : ('a, Out_channel.t, unit) format -> 'a
+
+(** Same as {!Out_channel.print_string} *)
+val print_string : string -> unit
 
 (** Same as {!Out_channel.print_endline} *)
 val print_endline : string -> unit
