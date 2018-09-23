f59a57771e721363d446c993569ee61094d0456c
David Allsopp <david.allsopp@metastack.com>
2018-09-23 14:55:10 +0100

    Improvements to build.sh

--- build.sh
+++ build.sh
@@ -9,15 +9,19 @@ if [ "$MODE" != "native" ]; then
   echo Forcing a local compiler to be built for $MODE
   WITH_OCAML=1
 elif [ -x "$(command -v ocamlc)" ]; then
-  if [ "$(ocamlc -version)" != "4.07.0" ]; then
+  if [ "$(ocamlc -version | tr -d '\015')" != "4.07.0" ]; then
     echo 'OCaml compiler detected is not 4.07.0, so building local version'
     WITH_OCAML=1
   else
     echo 'Using system OCaml 4.07 compiler'
   fi
 else
-  echo 'OCaml compiler not detected, building a local version of 4.07.0' 
-  WITH_OCAML=1
+  if [ -x $(pwd)/_obj/bin/ocamlc ] ; then
+    export PATH=$(pwd)/_obj/bin:$PATH
+  else
+    echo 'OCaml compiler not detected, building a local version of 4.07.0'
+    WITH_OCAML=1
+  fi
 fi
 
 if [ -x "$(command -v gmake)" ]; then
@@ -52,11 +56,17 @@ if [ $WITH_OCAML -eq 1 ]; then
   cd ../..
 fi
 
-if [ ! -e bootstrap/dune/Makefile -a -d .git ] ; then
-  git submodule update --init bootstrap/dune
-fi
 cd bootstrap/dune
-$MAKE
+if [ ! -x _build/install/default/bin/dune ] ; then
+  if [ ! -e Makefile -a -d ../../.git ] ; then
+    git submodule update --init .
+  fi
+#  git remote add dra27 https://github.com/dra27/dune.git -f
+#  git checkout dra27/let-syntax-crlf
+  $MAKE release
+else
+  ./boot.exe --release || (rm -f boot.exe && $MAKE)
+fi
 cd ../..
 ./bootstrap/dune/_build/install/default/bin/dune build --profile=release --debug-dependency-path @cli
 cp bootstrap/dune/_build/install/default/bin/dune _build/default/output/
